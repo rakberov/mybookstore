6569ee4af7719adce7f910fa8956d53d558423ac
Rovshan Akbarov <rakberovu@gmail.com>
2020-12-31 14:01:17 +0400

    fisrt commit

---
+++ src/main/java/mybookstore/mybookstore/MybookstoreApplication.java
@@ -0,0 +1,13 @@
+package mybookstore.mybookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mybookstore/mybookstore/MybookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mybookstore.mybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
