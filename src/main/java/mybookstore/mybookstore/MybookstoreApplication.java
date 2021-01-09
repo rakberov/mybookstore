aabc5e3edcb7dea9237078586b6837ef7bfae222
Rovshan Akbarov <rakberovu@gmail.com>
2021-01-10 00:17:47 +0400

    $m

--- src/main/java/mybookstore/mybookstore/MybookstoreApplication.java
+++ src/main/java/mybookstore/mybookstore/MybookstoreApplication.java
@@ -2,7 +2,6 @@ package mybookstore.mybookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class MybookstoreApplication {
